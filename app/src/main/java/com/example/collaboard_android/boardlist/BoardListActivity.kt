f68130e6654aeeb53f1c1a94b0c45ff376c1e724
heewon <ymcho24@naver.com>
2021-02-20 04:25:53 +0900

    [feat] 보드 리스트 뷰 현재 날짜 출력

--- app/src/main/java/com/example/collaboard_android/boardlist/BoardListActivity.kt
+++ app/src/main/java/com/example/collaboard_android/boardlist/BoardListActivity.kt
@@ -6,6 +6,8 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.collaboard_android.boardlist.adapter.BoardListAdapter
 import com.example.collaboard_android.boardlist.adapter.BoardListData
 import com.example.collaboard_android.databinding.ActivityBoardListBinding
+import java.lang.StringBuilder
+import java.util.*
 
 class BoardListActivity : AppCompatActivity() {
 
@@ -13,11 +15,15 @@ class BoardListActivity : AppCompatActivity() {
 
     private lateinit var boardListAdapter: BoardListAdapter
 
+    private lateinit var currentDate: Calendar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBoardListBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        setCurrentDateOnTextView()
 
         initRecyclerView()
     }
@@ -33,5 +39,32 @@ class BoardListActivity : AppCompatActivity() {
             BoardListData("STORM", "3 members")
         )
         boardListAdapter.notifyDataSetChanged()
+    }
+
+    private fun setCurrentDateOnTextView() {
+        currentDate = Calendar.getInstance()
+        val month = currentDate.get(Calendar.MONTH) + 1
+        val day = currentDate.get(Calendar.DAY_OF_MONTH)
+
+        binding.tvDate.text = getFormedDateString(month, day)
+    }
+
+    private fun getFormedDateString(month: Int, day: Int) : String {
+        val dayString: String = day.toString()
+        val monthString: String = when (month) {
+            1 -> "Jan"
+            2 -> "Feb"
+            3 -> "Mar"
+            4 -> "Apr"
+            5 -> "May"
+            6 -> "Jun"
+            7 -> "Jul"
+            8 -> "Aug"
+            9 -> "Sep"
+            10 -> "Oct"
+            11 -> "Nov"
+            else -> "Dec"
+        }
+        return "$monthString $dayString"
     }
 }
