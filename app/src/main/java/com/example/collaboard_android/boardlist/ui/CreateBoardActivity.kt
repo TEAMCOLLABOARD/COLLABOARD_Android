967985a0c394be56bd4c1a204ab6d4b5ea8027e9
heewon <ymcho24@naver.com>
2021-02-20 05:57:35 +0900

    [feat] 새 보드 만들기 뷰 구현

---
+++ app/src/main/java/com/example/collaboard_android/boardlist/ui/CreateBoardActivity.kt
@@ -0,0 +1,112 @@
+package com.example.collaboard_android.boardlist.ui
+
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.core.content.ContextCompat
+import com.example.collaboard_android.R
+import com.example.collaboard_android.databinding.ActivityCreateBoardBinding
+
+class CreateBoardActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCreateBoardBinding
+
+    private var selectRepo = -1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCreateBoardBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setKeyListenerOnEditText()
+
+        initEditImageButton()
+
+        initRepoSpinner()
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun initEditImageButton() {
+        binding.imgbtnEdit.setOnClickListener {
+            setEditTextEnabled()
+            showKeyboard()
+        }
+        binding.etBoardName.setOnTouchListener { _, _ ->
+            setEditTextEnabled()
+            showKeyboard()
+            true
+        }
+    }
+
+    private fun setEditTextEnabled() {
+        binding.etBoardName.apply {
+            requestFocus()
+            isFocusable = true
+            isCursorVisible = true
+            isCursorVisible = true
+            setSelection(this.length())
+        }
+    }
+
+    private fun showKeyboard() {
+        val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY)
+    }
+
+    private fun setKeyListenerOnEditText() {
+        binding.etBoardName.setOnKeyListener { _, keyCode, _ ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER || keyCode == EditorInfo.IME_ACTION_DONE) {
+                setEditTextDisabled()
+                hideKeyboard()
+                return@setOnKeyListener true
+            }
+            false
+        }
+    }
+
+    private fun setEditTextDisabled() {
+        binding.etBoardName.apply {
+            clearFocus()
+            isCursorVisible = false
+        }
+    }
+
+    private fun hideKeyboard() {
+        val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(binding.etBoardName.windowToken, 0)
+    }
+
+    private fun initRepoSpinner() {
+        val item = resources.getStringArray(R.array.label_array)
+
+        val repoAdapter = ArrayAdapter(this, R.layout.item_repo_spinner, item)
+        binding.spinnerRepo.adapter = repoAdapter
+
+        binding.spinnerRepo.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                //Todo: 사용자의 repo 목록 추가
+                selectRepo = when (position) {
+                    0 -> 0 // feature
+                    1 -> 1 // fix
+                    2 -> 2 // network
+                    3 -> 3 // refactor
+                    4 -> 4 // chore
+                    5 -> 5 // style
+                    else -> -1
+                }
+            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
+        }
+    }
+}
