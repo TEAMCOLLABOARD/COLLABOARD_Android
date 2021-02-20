dd603351ce0e6d6fd12b4085411c905b4e9c3fab
aerim <aerimforest98@gmail.com>
2021-02-21 01:02:52 +0900

    [feat] calendar 데이터 모델 추가

---
+++ app/src/main/java/com/example/collaboard_android/board/adapter/DeadlineDTO.kt
@@ -0,0 +1,9 @@
+package com.example.collaboard_android.board.adapter
+
+data class DeadlineDTO(
+    val creator: String? = "",
+    val content: String? = "",
+    val year: Int = 0,
+    val month: Int = 0,
+    val day: Int = 0
+)
