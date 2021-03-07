3b230faeddf6a22915be7514f8b5831801445c1e
aerim <aerimforest98@gmail.com>
2021-03-07 20:12:02 +0900

    [chore] deadline model 수정

--- app/src/main/java/com/example/collaboard_android/calendar/adapter/DeadlineDTO.kt
+++ app/src/main/java/com/example/collaboard_android/calendar/adapter/DeadlineDTO.kt
@@ -1,9 +1,7 @@
 package com.example.collaboard_android.calendar.adapter
 
 data class DeadlineDTO(
-    val creator: String? = "",
     val content: String? = "",
-    val year: Int = 0,
-    val month: Int = 0,
-    val day: Int = 0
+    val date: String? = "",
+    val label: Int? = 0
 )
