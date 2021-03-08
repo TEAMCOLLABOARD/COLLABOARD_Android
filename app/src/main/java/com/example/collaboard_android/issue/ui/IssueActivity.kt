68a9a230e4bd84abf7960a925c6fe69d3930c099
aerim <aerimforest98@gmail.com>
2021-03-08 15:52:51 +0900

    [feat] Get repository labels list

--- app/src/main/java/com/example/collaboard_android/issue/ui/IssueActivity.kt
+++ app/src/main/java/com/example/collaboard_android/issue/ui/IssueActivity.kt
@@ -4,11 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.example.collaboard_android.MainActivity
+import com.example.collaboard_android.R
 import com.example.collaboard_android.databinding.ActivityIssueBinding
+import com.example.collaboard_android.issue.data.IssueLabelsData
 import com.example.collaboard_android.issue.network.GitHubService
 import com.example.collaboard_android.issue.network.IssueDTO
+import com.example.collaboard_android.issue.network.RequestIssueLabels
 import com.example.collaboard_android.util.SharedPreferenceController
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody
@@ -23,11 +29,15 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class IssueActivity : AppCompatActivity() {
 
-    var server: GitHubService? = null
+    private lateinit var labelList: ArrayList<String>
+    private lateinit var assigneesList: ArrayList<String>
+
+    //    var server: GitHubService? = null
     private lateinit var binding: ActivityIssueBinding
 
     private var owner = ""
     private var repo = ""
+    var selectedLabels = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,6 +64,69 @@ class IssueActivity : AppCompatActivity() {
         binding.imgbtnBackIssue.setOnClickListener {
             finish()
         }
+
+        getIssueLabels()
+    }
+
+    private fun getIssueLabels() {
+        println("getIssueLabels 호출됨")
+
+        labelList = ArrayList()
+
+        // Retrofit 객체 생성
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.github.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        // retrofit 객체 통해 인터페이스 생성
+        val server: RequestIssueLabels = retrofit.create(RequestIssueLabels::class.java)
+
+        // 데이터 전송
+        server.getIssueLabels(
+            "Bearer ${SharedPreferenceController.getAccessToken(this)}",
+            owner, repo
+        )
+            .enqueue(object : Callback<ArrayList<IssueLabelsData.ResponseIssueLabelsDataItem>> {
+                // 데이터 전송에 실패한 경우
+                override fun onFailure(
+                    call: Call<ArrayList<IssueLabelsData.ResponseIssueLabelsDataItem>>?,
+                    t: Throwable?
+                ) {
+                }
+
+                // 데이터 전송 성공
+                override fun onResponse(
+                    call: Call<ArrayList<IssueLabelsData.ResponseIssueLabelsDataItem>>,
+                    response: Response<ArrayList<IssueLabelsData.ResponseIssueLabelsDataItem>>
+                ) {
+                    response.takeIf { it.isSuccessful }
+                        ?.body()
+                        ?.let {
+
+                            for (i in 0 until it.size) {
+                                labelList.add(it[i].name)
+                            }
+                            labelsAdapter()
+
+                        } ?: showError(response.errorBody())
+                }
+            })
+    }
+
+    private fun labelsAdapter() {
+        println("labelsAdapter 호출됨")
+        binding.constraintlayoutLabelsSpinner.visibility = View.VISIBLE
+        val labelsAdapter = ArrayAdapter(this, R.layout.item_label_spinner, labelList)
+        binding.spinnerLabels.adapter = labelsAdapter
+        binding.spinnerLabels.setSelection(0)
+
+        binding.spinnerLabels.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                selectedLabels = labelList[position]
+            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+        }
     }
 
     private fun creatIssue() {
@@ -64,7 +137,7 @@ class IssueActivity : AppCompatActivity() {
             .build()
 
         // retrofit 객체 통해 인터페이스 생성
-        server = retrofit.create(GitHubService::class.java)
+        val server: GitHubService = retrofit.create(GitHubService::class.java)
 
         // json 타입의 Body 생성
         val jsonObject = JSONObject()
@@ -75,11 +148,11 @@ class IssueActivity : AppCompatActivity() {
             jsonObject.toString().toRequestBody("application/json".toMediaTypeOrNull())
 
         // 데이터 전송
-        server?.createIssueCall(
+        server.createIssueCall(
             "Bearer ${SharedPreferenceController.getAccessToken(this)}",
             owner, repo, requestBody
         )
-            ?.enqueue(object : Callback<IssueDTO> {
+            .enqueue(object : Callback<IssueDTO> {
                 // 데이터 전송에 실패한 경우
                 override fun onFailure(call: Call<IssueDTO>?, t: Throwable?) {
                 }
